5b65e7a03f72314fc9d8df9c74d5268f46956a36
szrharrison <s.z.r.harrison@gmail.com>
2017-03-19 20:00:44 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  def initialize( word )
+    @word = word.chars.to_a
+  end
+  attr_reader :word
+  ######################################################
+  def match( strings_array )
+    strings_array.select do |string|
+      string_array = string.chars.to_a
+      string_array.sort == self.word.sort
+    end
+  end
+end
